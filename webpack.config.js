fed85ff71878b4d8e49562da7cf63202c1967295
Byron Hambly <byron.hambly@systemiclogic.net>
2016-05-12 16:50:07 +0200

    add tools and webpack config to build NPM module for publishing

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+// webpack.config.js
+
+var webpack = require('webpack')
+var path = require('path')
+var packageJson  = require('./package.json')
+
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+var libraryName = 'seen';
+var plugins = [], outputFile;
+
+if (env === 'build') {
+	plugins.push(new UglifyJsPlugin({ minimize: true }));
+	outputFile = libraryName + '.min.js';
+} else {
+	outputFile = libraryName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/dist/' + packageJson.version + '/seen.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel?compact=false',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js']
+  }
+}
+
+module.exports = config
